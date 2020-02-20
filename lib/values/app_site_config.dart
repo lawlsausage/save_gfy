938b22a8df1d77fda6c04746a75dcf1676dc960b
Christopher Kuo <>
2020-02-20 17:36:49 -0500

    WIP: Add app_site_config.dart
    - Create tests for app_site_config.dart.

--- lib/values/app_site_config.dart
+++ lib/values/app_site_config.dart
@@ -1,12 +1,15 @@
 class AppSiteConfig {
   AppSiteConfig({this.hosts});
 
+  /// Parses [Map]`<String, dynamic>` parameter [json] into an [AppSiteConfig] object.
+  ///
+  /// If [json] is `null`, [AppSiteConfig.fromJson] will return `null`.
   static AppSiteConfig fromJson(Map<String, dynamic> json) {
-    return AppSiteConfig(
-      hosts: (json != null && json is Map<String, dynamic>)
-          ? json['hosts']?.cast<String>()
-          : null,
-    );
+    return (json != null && json is Map<String, dynamic>)
+        ? AppSiteConfig(
+            hosts: (json['hosts'] ?? []).cast<String>(),
+          )
+        : null;
   }
 
   final List<String> hosts;
