6f323baee3b13110a8005325ac08de4b284261f4
Christopher Kuo <>
2020-02-21 11:26:26 -0500

    WIP: Add reddit_video_metadata_test.dart
    - Create tests for reddit_video_metadata.dart.
    - Account for malformed JSON situations and handle gracefully.

--- lib/values/reddit/reddit_video_metadata.dart
+++ lib/values/reddit/reddit_video_metadata.dart
@@ -1,3 +1,4 @@
+import 'package:save_gfy/services/logger_service.dart';
 import 'package:save_gfy/services/reddit_service.dart';
 import 'package:save_gfy/values/download_info.dart';
 import 'package:save_gfy/values/download_type.dart';
@@ -6,39 +7,45 @@ class RedditVideoMetadata {
   RedditVideoMetadata({this.dashPlaylistUrl, this.downloadInfoList});
 
   static RedditVideoMetadata fromJson(dynamic json) {
-    final rootListings = json as List<dynamic>;
+    final rootListings = json is List<dynamic> ? json : null;
 
     if (rootListings == null) {
       return null;
     }
 
-    final firstListing = rootListings.first as Map<String, dynamic>;
-    final data = firstListing['data'];
-    final children = data['children'] as List<dynamic>;
-    final firstChild = children.first as Map<String, dynamic>;
-    final firstChildData =
-        firstChild['data'] as Map<String, dynamic> ?? const {};
-    final secureMedia =
-        firstChildData['secure_media'] as Map<String, dynamic> ?? const {};
-    final redditVideo =
-        secureMedia['reddit_video'] as Map<String, dynamic> ?? const {};
-    final fallbackUrl = redditVideo['fallback_url'] as String ?? '';
-    final dashPlaylistUrl = redditVideo['dash_url'] as String ?? '';
-    final downloadInfoList = fallbackUrl.length > 0
-        ? [
-            DownloadInfo(
-              type: DownloadType.mp4,
-              name:
-                  '${RedditService.parseDownloadName(fallbackUrl)}${RedditService.videoFileSuffix}.mp4',
-              url: fallbackUrl,
-            ),
-          ]
-        : null;
+    try {
+      final firstListing = rootListings.first as Map<String, dynamic>;
+      final data = firstListing['data'];
+      final children = data['children'] as List<dynamic>;
+      final firstChild = children.first as Map<String, dynamic>;
+      final firstChildData =
+          firstChild['data'] as Map<String, dynamic> ?? const {};
+      final secureMedia =
+          firstChildData['secure_media'] as Map<String, dynamic> ?? const {};
+      final redditVideo =
+          secureMedia['reddit_video'] as Map<String, dynamic> ?? const {};
+      final fallbackUrl = redditVideo['fallback_url'] as String ?? '';
+      final dashPlaylistUrl = redditVideo['dash_url'] as String ?? '';
+      final downloadInfoList = fallbackUrl.length > 0
+          ? [
+              DownloadInfo(
+                type: DownloadType.mp4,
+                name:
+                    '${RedditService.parseDownloadName(fallbackUrl)}${RedditService.videoFileSuffix}.mp4',
+                url: fallbackUrl,
+              ),
+            ]
+          : null;
 
-    return RedditVideoMetadata(
-      dashPlaylistUrl: dashPlaylistUrl,
-      downloadInfoList: downloadInfoList,
-    );
+      return RedditVideoMetadata(
+        dashPlaylistUrl: dashPlaylistUrl,
+        downloadInfoList: downloadInfoList,
+      );
+    } catch (err) {
+      loggerService.d('Error occurred when parsing JSON', err);
+    }
+
+    return null;
   }
 
   final String dashPlaylistUrl;
